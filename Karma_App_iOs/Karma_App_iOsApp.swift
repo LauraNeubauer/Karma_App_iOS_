832e2a355bf795671f2e5d74e57c7dabf01255dd
lauraneubauer <malenundzeichnen@hotmail.de>
2024-02-22 11:19:29 +0100

    Initial Commit

---
+++ Karma_App_iOs/Karma_App_iOsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Karma_App_iOsApp.swift
+//  Karma_App_iOs
+//
+//  Created by Laura on 22.02.24.
+//
+
+import SwiftUI
+
+@main
+struct Karma_App_iOsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
