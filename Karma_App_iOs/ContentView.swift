61d539b41298c845f7d9016ab30610cfc3df51b1
lauraneubauer <malenundzeichnen@hotmail.de>
2024-03-04 20:38:26 +0100

    start_mainView

--- Karma_App_iOs/ContentView.swift
+++ Karma_App_iOs/ContentView.swift
@@ -1,4 +1,4 @@
-//
+
 //  ContentView.swift
 //  Karma_App_iOs
 //
@@ -9,13 +9,50 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack (alignment: .center) {
+                HStack {
+                    Text("Karma")
+                        .font(.custom("Avenir", size: 40))
+                        .fontWeight(.bold)
+                        .foregroundColor(.black)
+                        .padding(.leading, 20)
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Spacer()
+                    Button(action: {
+
+                    }, label: {
+                        Image(systemName: "gear")
+                            .foregroundColor(.green)
+                            .frame(width: 35, height: 35)
+                            .background(Color.black)
+                            .cornerRadius(10)
+                    })
+                    Button(action: {
+
+                    }, label: {
+                        Image(systemName: "person.circle")
+                            .foregroundColor(.green)
+                            .frame(width: 35, height: 35)
+                            .background(Color.black)
+                            .cornerRadius(10)
+                    })
+                    Button(action: {
+
+                    }, label: {
+                        Image(systemName: "bell")
+                            .foregroundColor(.green)
+                            .frame(width: 35, height: 35)
+                            .background(Color.black)
+                            .cornerRadius(10)
+                    })
+                    Spacer()
+                }
+                Spacer()
+            }
         }
-        .padding()
     }
 }
 
